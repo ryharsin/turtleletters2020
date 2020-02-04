5b3b19b33a4fe4c78bed47ea9838cf86491e6830
ayejeff <60612037+ayejeff@users.noreply.github.com>
2020-02-04 09:35:17 -0500

    Update turletters.py

--- turletters.py
+++ turletters.py
@@ -43,27 +43,92 @@ def turtleLetter(letter,tur):
     elif letter == "D":
 	    pass
     elif letter == "E":
-	    pass
+	tur.setheading(0)
+	tur.pendown()
+	tur.back(40)
+	tur.left(270)
+	tur.forward(25)
+	tur.left(90)
+	tur.forward(25)
+	tur.back(25)
+	tur.left(270)
+	tur.forward(25)
+	tur.left(90)
+	tur.forward(40)
+	tur.penup()
+	tur.forward(10)
+	tur.pendown()
     elif letter == "F":
 	    pass
     elif letter == "G":
-	    pass		
+	tur.setheading(0)
+	tur.pendown()
+	tur.left(90)
+	tur.forward(20)
+	tur.left(90)
+	tur.forward(20)
+	tur.back(20)
+	tur.left(90)
+	tur.forward(20)
+	tur.right(90)
+	tur.forward(20)
+	tur.right(45)
+	tur.forward(30)
+	tur.right(45)
+	tur.forward(20)
+	tur.right(45)
+	tur.forward(20)
+	tur.right(45)
+	tur.forward(25)
+	tur.penup()
+	tur.forward(10)		
     elif letter == "H":
 	    pass
     elif letter == "I":
-	    pass
+	tur.setheading(0)
+	tur.pendown()
+	tur.left(90)
+	tur.forward(50)
+	tur.right(90)
+	tur.penup()
+	tur.forward(10)
     elif letter == "J":
 	    pass
     elif letter == "K":
 	    pass
     elif letter == "L":
-	    pass
+	tur.setheading(0)
+	tur.left(90)
+	tur.forward(50)
+	tur.back(50)
+	tur.right(90)
+	tur.forward(40)
+	tur.penup()
+	tur.forward(10)
     elif letter == "M":
 	    pass
     elif letter == "N":
-	    pass
+	tur.setheading(0)
+	tur.pendown()
+	tur.right(90)
+	tur.forward(50)
+	tur.back(50)
+	tur.left(30)
+	tur.forward(60)
+	tur.left(150)
+	tur.forward(53)
+	tur.back(53)
+	tur.right(90)
+	tur.penup()
+	tur.forward(50)
     elif letter == "O":
-	    pass
+	tur.setheading(0)
+	tur.circle(25)
+	tur.penup()
+	tur.left(180)
+	tur.forward(25)
+	tur.left(90)
+	tur.forward(10)
     elif letter == "P":
 	    pass		
     elif letter == "Q":
@@ -73,13 +138,35 @@ def turtleLetter(letter,tur):
     elif letter == "S":
 	    pass
     elif letter == "T":
-	    pass
+	tur.setheading(0)
+	tur.pendown()
+	tur.forward(40)
+	tur.back(20)
+	tur.right(90)
+	tur.forward(50)
+	tur.left(90)
+	tur.penup()
+	tur.forward(70)
+	tur.left(90)
+	tur.forward(25)
+	tur.pendown()
     elif letter == "U":
 	    pass
     elif letter == "V":
 	    pass
     elif letter == "W":
-	    pass
+	tur.setheading(0)
+	tur.right(90)
+	tur.forward(50)
+	tur.left(150)
+	tur.forward(40)
+	tur.right(120)
+	tur.forward(40)
+	tur.left(150)
+	tur.forward(50)
+	tur.penup()
+	tur.right(90)
+	tur.forward(50)
     elif letter == "X":
 	    pass
     elif letter == "Y":
