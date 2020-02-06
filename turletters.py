fd94f2f00713671d11e91f3a30478ee3d22e2f52
ryharsin <60612006+ryharsin@users.noreply.github.com>
2020-02-06 08:54:43 -0500

    Update turletters.py

--- turletters.py
+++ turletters.py
@@ -59,101 +59,19 @@ def turtleLetter(letter,tur):
     elif letter == "L":
 	    pass
     elif letter == "M":
-	tur.penup()
-	tur.setpos(-35,-40)
-	tur.right(270)
-	tur.pendown()
-	tur.forward(40)
-	tur.right(135)
-	tur.forward(28)
-	tur.left(100)
-	tur.forward(28)
-	tur.right(145)
-	tur.forward(40)
-	tur.clear()
-	tur.penup()
-	
-	
+	pass
     elif letter == "N":
-	tur.penup()
-	tur.setpos(-35,-40)
-	tur.left(180)
-	tur.pendown()
-	tur.forward(40)
-	tur.right(150)
-	tur.forward(47)
-	tur.left(150)
-	tur.forward(43)
-	tur.clear()
-
-	
+	pass
     elif letter == "O":
-	tur.penup()
-	tur.setpos(-35,-15)
-	tur.left(180)
-	tur.pendown()
-	for i in range(20):
-    		tur.forward(8)
-    		tur.left(18)
-	tur.clear()
-
-	
+	pass
     elif letter == "P":
-	tur.penup()
-	tur.setpos(-35,-40)
-	tur.right(90)
-	tur.pendown()
-	tur.forward(40)
-	tur.right(90)
-	for i in range(12):
-    		tur.forward(3)
-    		tur.right(16)
-	tur.clear()
-	
-	
+	pass
     elif letter == "Q":
-	tur.penup()
-	tur.setpos(-35,-40)
-	tur.right(175)
-	tur.pendown()
-	for i in range(23):
-    		tur.forward(8)
-    		tur.left(18)
-	tur.penup()
-	tur.left(90)
-	tur.forward(15)
-	tur.right(180)
-	tur.pendown()
-	tur.forward(30)
-	tur.clear()
-
+	pass
     elif letter == "R":
-	tur.penup()
-	tur.setpos(-35,-40)
-	tur.left(135)
-	tur.pendown()
-	tur.forward(40)
-	tur.right(90)
-	for i in range(12):
-    		tur.forward(3)
-    		tur.right(16)
-	tur.right(215)
-	tur.forward(30)
-	tur.clear()
-	
-	
+	pass
     elif letter == "S":
-	tur.penup()
-	tur.setpos(0,10)
-	tur.right(190)
-	tur.pendown()
-	for i in range(11):
-    		tur.forward(6)
-    		tur.left(20)
-	for i in range(13):
-		tur.forward(6)
-    		tur.right(19)
-
+	pass
     elif letter == "T":
 	    pass
     elif letter == "U":
